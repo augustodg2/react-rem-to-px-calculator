7a20accabcd6e2cd85e1466c26ddac782049955f
Augusto Dias <augustodg2@gmail.com>
2020-03-19 19:24:46 -0300

    Updates

---
+++ precache-manifest.0ff198aac864e18de4b74deb27b7d3dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b00279ac25b7ccaeaf75d825a4a34a7",
+    "url": "/rem-to-px-calculator/index.html"
+  },
+  {
+    "revision": "d6f30eeeb352ef1aae14",
+    "url": "/rem-to-px-calculator/static/css/main.a3a2a3a1.chunk.css"
+  },
+  {
+    "revision": "a2a7b4424d2f7ab6f586",
+    "url": "/rem-to-px-calculator/static/js/2.4412fe2e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/rem-to-px-calculator/static/js/2.4412fe2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6f30eeeb352ef1aae14",
+    "url": "/rem-to-px-calculator/static/js/main.b805292a.chunk.js"
+  },
+  {
+    "revision": "03e19b5685a3ebd049a9",
+    "url": "/rem-to-px-calculator/static/js/runtime-main.d5fb014e.js"
+  }
+]);
